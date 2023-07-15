6fb59abe358115c32180cb9bbf7c535f2a642a75
GT3CH1 <gavinpease@gmail.com>
2023-07-15 15:19:46 -0600

    Add MixinClientPlayerEntityFastMine

---
+++ src/main/java/com/peasenet/mixins/MixinClientPlayerEntityFastMine.java
@@ -0,0 +1,19 @@
+package com.peasenet.mixins;
+
+import com.peasenet.main.GavinsMod;
+import com.peasenet.mods.Type;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class MixinClientPlayerEntityFastMine {
+    @Inject(method = "getBlockBreakingSpeed", at = @At("RETURN"), cancellable = true)
+    public void checkFastMine(BlockState state, CallbackInfoReturnable<Float> ci) {
+        if (GavinsMod.isEnabled(Type.FAST_MINE))
+            ci.setReturnValue(500.0f);
+    }
+}
